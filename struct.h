f6e1c90147e0d44bbc9afef3288770c1e9d83d1c
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-04-01 14:54:29 +0000

    Tache B.3 First upload of struct.h

---
+++ struct.h
@@ -0,0 +1,63 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#define hand_size 8
+/**
+* \file interface.h
+*
+* \author Nathan VETTER
+* define the type of structures used in all the other files
+*/
+
+/* nom et ref a la fonction */
+typedef struct carte {
+    char* name;
+    int function_number;
+} carte;
+
+/* pile de carte */
+typedef struct cartePioche{
+  carte carte;
+  struct cartePioche *suivant;
+} *pioche;
+
+/* tableau */
+typedef struct hand{
+    carte hand[hand_size];
+} hand; 
+
+/* nbr de point, main, redraw, nbr de victoire*/
+typedef struct faction{
+    int nb_point;
+    hand hand;
+    int redraw;
+    int nb_v;
+} faction;  
+
+
+/* carte et bool sens */
+typedef struct cell{
+    carte carte;
+    int sens;
+    faction* fac;
+} cell; 
+
+/* matrice*/
+typedef struct grid{
+    int size;
+    cell** cases;
+} grid;  
+
+/* grid et 2 factions*/
+typedef struct plateau{
+    grid plateau;
+    faction fa;
+    faction fb;
+} plateau;  
+
+
+
+
+
+
+#endif
