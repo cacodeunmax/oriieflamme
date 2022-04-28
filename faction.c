835c3a250337914e2818d7640272b5299b55fa3b
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-04-28 15:09:07 +0000

    Tache B.5, getter et setter des factions ajoutés.

--- faction.c
+++ faction.c
@@ -2,16 +2,34 @@
 #define FACTION_C
 
 #include "faction.h"
+#include "struct.h"
+#include "struct.c"
 #include <stddef.h>
 #include <stdlib.h>
 
 /**
-* \file faction.h
+* \file faction.c
 *
 * \author Nathan VETTER
 * Implémente les fonctions liées aux factions.
 */
 
+
+int get_point(faction f){
+    return f.nb_point;
+}
+
+int get_vic(faction f){
+    return f.nb_v;
+}
+
+void set_point(faction f, int p){
+    f.nb_point = p;
+}
+
+void set_vic(faction f, int v){
+    f.nb_v=v;
+}
 
 int has_redrawn(faction f){
     return f.redraw;
