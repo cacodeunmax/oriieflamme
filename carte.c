6c7f0150571438d125ac61caed57526903540263
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-04-28 15:07:42 +0000

    Tache B.5, getter des cartes ajoutés.

--- carte.c
+++ carte.c
@@ -1,10 +1,21 @@
 #include "struct.c"
+#include "struct.h"
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "carte.h"
 
+
+char* get_name(carte c){
+    return c.name;
+}
+
+int get_func(carte c){
+    return c.function_number;
+}
 
 int equals_carte(carte c, carte v){
     return (strcmp(c.name,v.name)&&(c.function_number==v.function_number));
 }
+
