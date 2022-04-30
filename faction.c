c7da4cee2b8e04609f547b93cdff76613e93003e
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-04-30 18:13:36 +0000

    Tache B.5 new setter and getter and Tache B.6, implementing all functions

--- faction.c
+++ faction.c
@@ -1,11 +1,13 @@
 #ifndef FACTION_C
 #define FACTION_C
 
-#include "faction.h"
 #include "struct.h"
 #include "struct.c"
 #include <stddef.h>
 #include <stdlib.h>
+#include <time.h>
+#include "constante.h"
+#include "carte.c"
 
 /**
 * \file faction.c
@@ -23,31 +25,44 @@ int get_vic(faction f){
     return f.nb_v;
 }
 
+void add_point(faction f, int p){
+    f.nb_point += p;
+}
+
+void shuffle(faction f){
+    srand(time(NULL));
+    carte catalogue[DECK_SIZE];
+    create_catalogue(catalogue);
+
+    for (int i = 0; i < DECK_SIZE-1; ++i){
+        int j = rand() % (DECK_SIZE-i) + i;
+        carte temp = catalogue[i];
+        catalogue[i] = catalogue[j];
+        catalogue[j] = temp;
+    }
+
+    pioche piochef = create_empty_pioche();
+
+    for (int i=0; i< DECK_SIZE; i++){
+        pioche_add(catalogue[i], &piochef);
+    }
+
+    f.pioche= &piochef;
+
+
+
+}
+
 void set_point(faction f, int p){
     f.nb_point = p;
 }
 
 void set_vic(faction f, int v){
-    f.nb_v=v;
+    f.nb_v = v;
 }
 
 int has_redrawn(faction f){
     return f.redraw;
-}
-
-void redraw(faction f){
-    clearHand(f);
-    shuffle(f);
-    draw(f);
-}
-
-void clearHand(faction f){
-    int i;
-    carte c;
-    c.name="null";
-    for (i=0; i<8; i++){
-        f.hand.hand[i]=c;
-    }
 }
 
 void draw(faction f){
@@ -57,6 +72,22 @@ void draw(faction f){
         (f.hand.hand)[i]=depioche(pioche);
     }
 }
+
+void clearHand(faction f){
+    int i;
+    for (i=0; i<8; i++){
+        f.hand.hand[i]=CN;
+    }
+}
+
+void redraw(faction f){
+    clearHand(f);
+    shuffle(f);
+    draw(f);
+}
+
+
+
 
 
 #endif
