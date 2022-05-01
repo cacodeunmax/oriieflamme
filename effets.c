dfe3cede021288cf7ac3ed5ce0d5ec20d93f2f4f
Noemie TURMEL <noemie.turmel@ensiie.fr>
2022-05-01 12:19:59 +0000

    Replace effets.c

--- effets.c
+++ effets.c
@@ -18,16 +18,15 @@
 //#include "struct.c"
 
 
-/* a mettre dans carte.c*/
+/* a mettre dans carte.c
 int equals_carte(carte c, carte v){
     return (strcmp(c.name,v.name)&&(c.function_number==v.function_number));
 }
 
 
 #define N 32
-carte null={"null",-1};
+carte null={"null",-1};*/
 
-/*------------------------------------------------------------------------------------*/
 /**
 *\brief active la carte fise
 *\param p une une faction
@@ -172,7 +171,7 @@ void f_liiens(faction p,plateau m){
 
     carte cartes_retirees[hand_size*2];
     for (int k=0; k<(hand_size*2); k++){
-        cartes_retirees[k]=null;
+        cartes_retirees[k]=CN;
     }
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
@@ -188,7 +187,7 @@ void f_liiens(faction p,plateau m){
 
                 for (int k=0; k<(hand_size*2); k++){
 
-                    if     ( equals(current_carte,"Café")(cartes_retirees[k])){
+                    if     ( empty(cartes_retirees[k])){
                         cartes_retirees[k]=cc;
                         break;}
                 }
@@ -338,15 +337,65 @@ int eco;
     }
 }
 
-/*
-void f_reprographie(faction p, faction *q, plateau m){
-    liste_double lst;
-    
+/**
+*\brief active les effets de la carte reprographie
+*\param p une faction
+*\param m le plateau
+*\return nothing 
+*/
+void f_reprographie(faction p,plateau m){
 
-}*/
-void f_isolation(){
+    /*on initialise le tableau des occurences*/
+    int occ[33];
+    for (int k;k<33,k++){
+        occ[k]=0;
+    }
+
+    /*on boucle sur la grille pour récolter le nombre d'occurence de chaque carte*/
+    for (int i=0;i<N; i++){
+        for (int i=0;i<N; i++){
+            carte cc=get_c_carte(m,i,j);
+            int sens=get_c_sens(m,i,j);
+            if (!empty(cc)){
+                int x=get_func(cc);
+                occ[x]++;
+            }
+        }
+    }
+
+    /*dénombrement : le nombre de paires d'un ensemble fini à n éléments est égal à n(n – 1)/2  */
+    int n;
+    faction adv=get_adverse(p);
+    for (int k;k<33,k++){
+        n=occ[k];
+        nb=n*(n-1)/2;
+        add_point(adv,(-nb));
+    }
 
 }
+
+
+/**
+*\brief active les effets de la carte isolation
+*\param p une faction
+*\param m le plateau
+*\return nothing 
+*/
+void f_isolation(plateau p){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            carte cc=get_c_carte(m,i,j);
+            int sens=get_c_sens(m,i,j);
+            faction f=get_c_fac(m,i,j);
+            if ((sens==0)&&(!empty(cc))){
+                add_point(f,1);
+            }
+        }
+    }
+}
+
+
+
 /**
 *\brief active les effets de la carte sobriété
 *\param p une faction
@@ -381,13 +430,13 @@ void f_sobriete(plateau m){
 *\return nothing 
 */
 void f_heure(faction p,plateau m){
+    faction adv=get_adverse(m,f);
 
     for (int i=1; i<N; i++){
         for (int j=0; j<N; j++){
             carte current_carte = get_c_carte(m,i,j);
             int current_sens=get_c_sens(m,i,j);
             if ((equals(current_carte,"Heures supplémentaires")&&(current_sens==1))){
-                faction adv=get_adverse(m,f);
                 add_point(adv,(-3));
             }
         }
@@ -442,7 +491,7 @@ void f_KB(faction p,plateau m){
 *\param m le plateau
 *\return nothing 
 */
-void f_KH(faction p,plateau m){
+void f_KG(faction p,plateau m){
     int count=0;
 
     while(count==0){
@@ -475,9 +524,25 @@ void f_MM(faction p,plateau m, carte c){}
 
 
 
+/**
+*\brief active l'effet de la carte Vitera Y
+*\param p une faction
+*\param m le plateau
+*\param c la dernière carte qui a été retournée 
+*\return nothing 
+*/
 
+void f_VY(faction p,plateau m){
+    faction q=get_adverse(p);
+    if (get_point(p)>get_point(q)){
+        add_point(q,3);
 
-
+    }
+    /* si les 2 factions ont le meme nb de points personne n'en gagne avec cette carte*/
+    if (get_point(p)<get_point(q)){
+        add_point(p,3);
+    }
+}
 
 
 /**
@@ -619,7 +684,7 @@ void f_ALL(faction p,plateau m){
 *\return nothing 
 */
 
-void f_ALL(faction p){
+void f_GB(faction p){
     faction q=get_adverse(p);
     if (get_point(p)>=get_point(q)){
         add_point(p,3);
@@ -772,7 +837,7 @@ void f_DW(faction p, plateau m){
 *\return nothing 
 */
 
-void f_DW(faction p, plateau m, int x){
+void f_DADC(faction p, plateau m, int x){
     int count=0;   
     //premières boucles sur le plateau, on cherche forest
     for (int j=0; j<N; j++){
@@ -799,8 +864,6 @@ void f_DW(faction p, plateau m, int x){
 *\param x l'entier numéro de ligne de la carte
 *\return nothing 
 */
-
-
 
 
 
@@ -839,4 +902,154 @@ void f_LP(plateau m, int x, int y, faction p){
     if (fisa>=1){
         add_point(p,5);
     }
+}
+
+
+/**
+*\brief active l'effet de la carte Katrin Salhab
+*\param p un pointeur vers la faction qui pose la carte
+*\param m le plateau
+*\param x l'entier numéro de ligne de la carte
+*\param y l'entier numéro de colonne de la carte
+*\return nothing 
+*/
+
+void f_KS(faction p, plateau m, int x){
+
+    int DADC=0;
+    int EL=0;
+    int LP=0;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            cc=get_c_carte(m,i,j);
+            sens=get_c_sens(m,i,j);
+            if (equals(cc,"Djibril-Aurélien Djembele-Cabeau")&&(sens==1)){
+                DADC=1;
+            }
+            if (equals(cc,"Lucienne Pacavé")&&(sens==1)){
+                LP=1;
+            }
+            if (equals(cc,"Eric Lejeune")&&(sens==1)){
+                EL=1;
+            }
+            if (DADC+EL+LP==3){break;}
+        }
+    if (DADC+EL+LP==3){break;}
+    }
+    if (DADC+EL+LP==3){add_point(p,10);}
+    else{
+        for(int k=0; k<N;k++){
+            set_c_nulle(m,x,k);
+        }
+    }
+}
+
+
+
+
+/**
+*\brief active l'effet de la carte Laurent Prevel
+*\param p un pointeur vers la faction qui pose la carte
+*\param m le plateau
+*\param x l'entier numéro de ligne de la carte
+*\param y l'entier numéro de colonne de la carte
+*\return nothing 
+*/
+
+void f_PREVEL(faction p, plateau m){
+    //on va d'abord vérifié si PREVEL elle la dernière carte qui est retournée
+    int last=1;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            cc=get_c_carte(m,i,j);
+            sens=get_c_sens(m,i,j);
+            if((!empty(cc))&&(sens==0)){
+                last=0;
+                break;
+            }
+        }
+    if (last==0){break;}
+    }
+
+    if(last==1){ //cad Laurent prevrel est bien la dernière carte qui a été retournée
+        set_victory_manche(p,m);
+    }
+}
+
+
+
+
+
+
+/**
+*\brief active l'effet de la carte c
+*\param p un pointeur vers la faction qui pose la carte
+*\param m le plateau
+*\param x l'entier numéro de ligne de la carte
+*\param y l'entier numéro de colonne de la carte
+*\return nothing 
+*/
+void active(carte c, faction* p, plateau* m,int i, int j){
+    int n=get_func(c);
+    switch(n)
+    {
+        case 1:
+        f_fise(p);
+        case 2:
+        f_fisa(p,m);   
+        case 3:
+        f_fc(p,m);
+        case 4: 
+        f_ecologiie(p,m);
+        
+        case 5:
+        f_liiens(p,m);
+        case 6 :
+        f_ssa(m);
+        case 7 :
+        f_alcool(m,i,j);
+        case 8 :
+        f_cafe(p,m);
+        
+        case 9 : 
+        f_the(p,m);
+        
+        case 10 : ; //ecocup
+        case 11: 
+        f_reprographie(p,m);
+        case 12 : 
+        f_isolation(m);
+        case 13 :
+        f_sobriete(m);
+        case 14: 
+        f_heure(p,m);
+        case 15: f_KB(p,m);
+        case 16 :
+        f_KG(p,m);
+        case 17:
+        f_MM(p,m,c);
+        case 18 :
+        f_VY(p,m);
+        case 19: f_JS(p,m);
+
+        case 20:
+        f_FB(p,m);
+        case 21: 
+        f_CD(p,m,i,j);
+        case 22: f_ALL(p,m);
+        case 23: f_GB(m);
+        case 24: f_CM(m);
+        case 25: f_TM(p,m);
+        case 26: f_JS(p,m);
+        case 27: f_DW(p,m);
+        case 28: f_DADC(p,m,i);
+        case 29: f_EL();
+        case 30: f_LP(m,i,j,p);
+        case 31: f_KS(p,m,i);
+        case 32: f_PREVEL(f,m);
+        case default : 
+    }
+
+
+
 }
