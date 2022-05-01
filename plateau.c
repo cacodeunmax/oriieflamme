868f369d788bc08f5a50a2f4aab693889af44de8
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-05-01 12:09:36 +0000

    bug fix

--- plateau.c
+++ plateau.c
@@ -54,10 +54,13 @@ void set_c_fac(plateau p, int x, int y, faction* f){
     p.plateau.cases[x][y].fac = f;
 }
 
+void set_victory(faction *f){
+    f->nb_v ++;
+}
 
 plateau create_plateau(){
     plateau p;
-    p.plateau = grid_create(31);
+    p.plateau = grid_create(4*HAND_SIZE-1);
     int i, j;
     for (i = 0; i<p.plateau.size; i++){
         for (j=0; j< p.plateau.size; j++){
@@ -71,14 +74,18 @@ plateau create_plateau(){
     fa.nb_v=0;
     fa.nb_point=0;
     fa.redraw=0;
-    clearHand(fa);
-    shuffle(fa);
+    printf("cas fa\n");
+    clearHand(&fa);
+    shuffle(&fa);
+    printf("fin cas fa\n\n");
 
     fb.nb_v=0;
     fb.nb_point=0;
     fb.redraw=0;
-    clearHand(fb);
-    shuffle(fb);
+    printf("cas fb\n");
+    clearHand(&fb);
+    shuffle(&fb);
+    printf("fin cas fb\n\n");
 
     p.fa = fa;
     p.fb = fb;
@@ -125,6 +132,7 @@ carte flip_card(plateau *p){
         }
     }
     //connec_carte(*p, f, c.function_number);
+    last_card = c.function_number;
     return c;
 }
 
@@ -132,14 +140,14 @@ carte flip_card(plateau *p){
 void victory_manche(plateau* p){
     int i, j;
     if ( p -> fa.nb_point > p-> fb.nb_point){
-        p-> fa.nb_v += 1;
+        set_victory( &p -> fa);
     } else if ( p -> fa.nb_point < p-> fb.nb_point){
-        p-> fb.nb_v += 1;
+        set_victory( &p -> fb);
     } else {
         for (i = 0; i <31; i++){
         for (j=0; j< 31; j++){
-            if ((!strcmp(p-> plateau.cases[i][j].carte.name, "null"))){
-                p-> plateau.cases[i][j].fac->nb_point+=1;
+            if ((!empty(p-> plateau.cases[i][j].carte))){
+                set_victory( p-> plateau.cases[i][j].fac);;
                 goto END;
             }
         }
@@ -150,43 +158,52 @@ void victory_manche(plateau* p){
     END:;
 }
 
-int initialiser_manche(plateau* p, faction* f1, faction* f2){
-    if (f1 == &FN){
-        goto RAND;
+int initialiser_manche(plateau p, faction* f1, faction* f2){
+    if (f1 == NULL){          
+        int r = rand() % 2;
+        if (r){
+            f1 = &p.fa;
+            f2 = &p.fb;
+        } else {
+            f1 = &p.fb;
+            f2 = &p.fa;  
+        }
+        
     } else if ((f1 -> nb_v == MG) || (f2 -> nb_v == MG)){
         return 1;
     
     }else if (!(f1-> nb_point + f2 -> nb_point)%2){
-        goto RAND;
+        int r = rand() % 2;
+        if (r){
+            f1 = &p.fa;
+            f2 = &p.fb;
+        } else {
+            f1 = &p.fb;
+            f2 = &p.fa;  
+        }
 
     } else {
         faction temp = *f1;
-        *f1 = *f2;
-        *f2= temp;
+        f1 = f2;
+        f2= &temp;
     }
 
-    
-
-    RAND:;
-        int r = rand() % 2;
-        if (r){
-            *f1 = p-> fa;
-            *f2 = p-> fb;
-        } else {
-            *f1 = p-> fb;
-            *f2 = p-> fa;  
-        }
-        
 
     f1 -> nb_point=0;
     f2-> nb_point=0;
-    clearHand(*f1);
-    shuffle(*f1);
-    clearHand(*f2);
-    shuffle(*f2);
+    clearHand(f1);
+    shuffle(f1);
+    draw(f1);
+    clearHand(f2);
+    shuffle(f2);
+    draw(f2);
 
-    grid_free(p->plateau);
-    p-> plateau = grid_create( 4* HAND_SIZE - 1);
+    int i, j;
+    for (i = 0; i<p.plateau.size; i++){
+        for (j=0; j< p.plateau.size; j++){
+            p.plateau.cases[i][j]=CLN;
+        }
+    }
     return 0;
 }
 
