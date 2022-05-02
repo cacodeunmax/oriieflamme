a6aa18dd3aa2804cc8622741072e07e23544d71c
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-05-02 19:11:48 +0000

    Tache B.8, Final commit

--- interface.c
+++ interface.c
@@ -1,8 +1,7 @@
 #ifndef INTERFACE_C
 #define INTERFACE_C
 
-
-#include "struct.c"
+#include <stdio.h>
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -10,6 +9,7 @@
 #include "struct.h"
 #include "constante.h"
 #include "plateau.c"
+#include "carte.c"
 
 
 
@@ -52,38 +52,38 @@ carte chose_card(faction *f){
 }
 
 int carte_adj(plateau p, int x, int y){
-    int max = 4 * HAND_SIZE -1;
+    int max = 4 * HAND_SIZE -2;
     if ((x ==0 && y ==0)||(x==0 && y == max ) || (x == max && y == 0) || (x ==max && y == max)){
         return 0;
     } else if (x == 0) {
-        if (y == max/2 -1 && !equals_carte( get_c_carte(p, x+1, y), CN)) {
+        if (y == max/2  && !equals_carte( get_c_carte(p, x+1, y), CN)) {
             return 1;
         }
         else { 
             return 0;
         }
     } else if (x == max){
-        if (y == max/2 -1 && !equals_carte( get_c_carte(p, x-1, y), CN)){
+        if (y == max/2  && !equals_carte( get_c_carte(p, x-1, y), CN)){
             return 1;
         }
         else { 
             return 0;
         }
     } else if (y == 0) {
-        if (x == max/2 -1 && !equals_carte( get_c_carte(p, x, y+1), CN)) {
+        if (x == max/2  && !equals_carte( get_c_carte(p, x, y+1), CN)) {
             return 1;
         }
-        else { 
+        else {
             return 0;
         }
     } else if (y == max){
-        if (x == max/2 -1 && !equals_carte( get_c_carte(p, x, y-1), CN)) {
+        if (x == max/2 && !equals_carte( get_c_carte(p, x, y-1), CN)) {
             return 1;
         }
         else { 
             return 0;
         }
-    } else if (!equals_carte( get_c_carte(p, x-1, y-1), CN) || !equals_carte( get_c_carte(p, x+1, y-1), CN) || !equals_carte( get_c_carte(p, x-1, y+1), CN) || !equals_carte( get_c_carte(p, x+1, y+1), CN)){
+    } else if (!empty( get_c_carte(p, x, y-1)) || !empty( get_c_carte(p, x+1, y)) || !empty( get_c_carte(p, x-1, y)) || !empty( get_c_carte(p, x, y+1))){
             return 1;
         } else { 
             return 0;
@@ -95,15 +95,13 @@ void chose_pos(plateau p, int* x, int* y){
     if (empty(get_c_carte(p, 2 * HAND_SIZE-1, 2 * HAND_SIZE -1))){
         printf("%3d %3d\n", 2 * HAND_SIZE-1, 2 * HAND_SIZE -1);
     } else {
-        printf("test 76:\n");
-        /*for (int i =0; i < 4 * HAND_SIZE -1; i++){
+        for (int i =0; i < 4 * HAND_SIZE -1; i++){
             for (int j =0; j < 4 * HAND_SIZE -1; j++){
-                printf("test 78: %i\n", 31*i + j );
-                if (carte_adj(p, i, j)){
+                if (carte_adj(p, i, j) && empty(get_c_carte(p, i,j))){
                     printf("%3d %3d\n", i, j);
                 }
             }
-        }*/
+        }
     }
     printf("Ligne:\n");
     scanf("%d", x);
