e19d7a2a050ef2871af03f362d2b6e51e8695716
Nathan VETTER <nathan.vetter@ensiie.fr>
2022-06-05 16:04:26 +0000

    Tache B.4 Non-numerical constants and global variables

--- constantenn.h
+++ constantenn.h
@@ -1,4 +1,3 @@
-
 #ifndef CONSTANTENN_H
 #define CONSTANTENN_H
 #include "struct.h"
@@ -6,6 +5,7 @@
  carte CN = { "null", -1, "carte nulle"};
  faction FN = {};
  cell CLN = {{ "null", -1, "carte nulle"}, 0, NULL};
+
 
 int prevel;
 
