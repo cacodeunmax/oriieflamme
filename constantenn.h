a0a1a957eadb42282ce24ea010e99fde32204d3f
Noemie TURMEL <noemie.turmel@ensiie.fr>
2022-06-06 18:54:02 +0000

    Tâche B2

---
+++ constantenn.h
@@ -0,0 +1,12 @@
+
+#ifndef CONSTANTENN_H
+#define CONSTANTENN_H
+#include "struct.h"
+
+ carte CN = { "null", -1, "carte nulle"};
+ faction FN = {};
+ cell CLN = {{ "null", -1, "carte nulle"}, 0, NULL};
+
+int prevel=0;
+int merabet=0;
+#endif
