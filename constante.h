0bc05be29fe18707e28b67a8e784c170159680dc
Noemie TURMEL <noemie.turmel@ensiie.fr>
2022-06-06 18:48:21 +0000

    Replace constante.h

--- constante.h
+++ constante.h
@@ -2,19 +2,16 @@
 #define CONSTANTE_H
 
 
-#include "struct.h"
 
-#define N 32
+#define N 31
 #define HAND_SIZE 8
 #define DECK_SIZE 47
 
 #define MG 2 
 
-const carte CN = { "null", -1, "carte nulle"};
-const faction FN = {};
-const cell CLN = {{ "null", -1, "carte nulle"}, 0, NULL};
 
-int last_card;
-int prevel;
+
+//int last_card;
+//int prevel;
 
 #endif
